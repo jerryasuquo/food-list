c8740f33ce3872400e659a105de01757505a1ce8
Jeremiah Bassey Asuquo <jbassuquo4@gmail.com>
2021-07-10 04:08:58 +0100

    Added search query filter implementation commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import CardList from "./components/CardList";
 import { data } from "./components/array";
 
 export default class App extends React.Component {
+  /*
+
+    using constructor fn that takes in props.
+      using super method that takes in props.
+      set state to equal an object with a meals key, and pass it its value from the exported data object.
+
+    using componentDidMount() 
+     using fetch() to collect data from an external server, using .then() which takes in an anon callback fn as a parameter, and give the anon fn a 'data' params which is the data being fetched, then return the data and jsonify it using .json()
+     using .then() again which takes in a callback fn that has a jsonData params, which is the response received, and returns the jsonData
+       using .setState() to update our state with the data collected.
+       using .catch() which takes in a callback fn that has an error params, and console.log() the err, to catch any errors.
+
+    return a cardlist child component which takes in myMeals as props, and set the value to be this.state.meals
+  */
+
   constructor(props) {
     super(props);
     this.state = {
       meals: data.meals,
+      searchQuery: "",
     };
   }
 
@@ -22,10 +38,33 @@ export default class App extends React.Component {
       .catch((err) => console.log(err));
   }
 
+  handleChange = (event) => {
+    this.setState(
+      {
+        searchQuery: event.target.value,
+      },
+      () => {
+        console.log("Query Test", this.state.searchQuery);
+      }
+    );
+  };
+
   render() {
+    const filterMeals = this.state.meals.filter((meal) => {
+      return meal.strMeal.toLowerCase().includes(this.state.searchQuery);
+    });
+
     return (
       <div id="app-body">
-        <CardList myMeals={this.state.meals} />
+        <form id="search-box">
+          <input
+            type="text"
+            placeholder="search..."
+            id="search-query"
+            onChange={this.handleChange}
+          />
+        </form>
+        <CardList myMeals={filterMeals} />
       </div>
     );
   }
